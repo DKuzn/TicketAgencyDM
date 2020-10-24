97c1e98f099639443060449a1f258f4eaf211cf1
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-10-24 13:45:16 +0300

    update GUI implementation

--- src/main.py
+++ src/main.py
@@ -1,16 +1,36 @@
 import sys
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets
+from sql_utils import *
+import mainwindow
 
 
-class SolveApp(QtWidgets.QMainWindow):
+class TicketAgencyApp(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
     def __init__(self):
-        super(SolveApp, self).__init__()
-        uic.loadUi('../resources/mainwindow.ui', self)
+        super(TicketAgencyApp, self).__init__()
+        self.setupUi(self)
+        self.payButton.clicked.connect(self.pay_button_clicked)
+        self.addToOrderButton.clicked.connect(self.add_to_order_button_clicked)
+        self.formTicketsButton.clicked.connect(self.form_ticket_button_clicked)
+        self.eventSiteChoice.addItems(event_sites_list())
+        self.eventSiteChoice.activated.connect(self.event_site_choose)
+
+    def pay_button_clicked(self):
+        pass
+
+    def add_to_order_button_clicked(self):
+        pass
+
+    def form_ticket_button_clicked(self):
+        pass
+
+    def event_site_choose(self):
+        self.eventChoice.clear()
+        self.eventChoice.addItems(events_list(self.eventSiteChoice.currentText()))
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    window = SolveApp()
+    window = TicketAgencyApp()
     window.show()
     app.exec_()
 
