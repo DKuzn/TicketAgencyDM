035c3a656531b159048873b319a35adc9cf4cc91
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-12-07 19:40:36 +0300

    update get_a4 function

--- src/form_tickets.py
+++ src/form_tickets.py
@@ -16,44 +16,61 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import qrcode
+import textwrap
 from sql_utils import get_ticket_info
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 
 def get_qrcode(text: str):
     img = qrcode.make(text)
     img = img.get_image()
-    img = img.resize((800, 800))
+    img = img.resize((600, 600))
     return img
 
 
 def form_ticket_info(ticket: int):
     info = get_ticket_info(ticket)
     header = 'Тип: Название: Площадка: Дата: Время: Ряд: Место: Цена:'.split(' ')
-    to_info = ['Билетное агенство "Кузница"\n', 'Номер билета: ' + str(ticket) + '\n']
+    to_info = ['\n'.join(('Билетное агенство "Кузница"', 'Номер билета: ' + str(ticket)))]
     for i, inf in enumerate(info):
-        row = [header[i], ' ', str(inf), '\n']
+        row = ''.join((header[i], ' ', str(inf)))
+        row = textwrap.wrap(row, width=26)
         to_info += row
-    formed_info = ''.join(to_info)
+    formed_info = '\n'.join(to_info)
     return formed_info
 
 
 def get_a4(tickets: list):
-    qr_codes = []
+    template = Image.open('../resources/ticket_template.png').convert('RGB')
+    images = []
+    backs = []
+    font = ImageFont.truetype('../resources/Montserrat-SemiBold.ttf', size=36)
     for i in tickets:
-        img = get_qrcode(form_ticket_info(int(i)))
-        qr_codes.append(img)
-    background = Image.new('L', (2480, 3508), color=255)
+        info = form_ticket_info(int(i))
+        qr_code = get_qrcode(info)
+        img = Image.new('RGB', (1600, 800), color=(255, 255, 255))
+        img.paste(template, (0, 0))
+        img.paste(qr_code, (900, 100))
+        draw = ImageDraw.Draw(img)
+        draw.text((200, 150), text=info, font=font, fill=(0, 0, 0))
+        images.append(img)
+    background1 = Image.new('RGB', (3508, 2480), color=(255, 255, 255))
+    backs.append(background1)
+    background2 = Image.new('RGB', (3508, 2480), color=(255, 255, 255))
+    backs.append(background2)
     qr = 0
-    for y in range(0, background.size[1] - 800, 800):
-        for x in range(0, background.size[0] - 800, 800):
-            if qr < len(qr_codes):
-                background.paste(qr_codes[qr], (x, y))
-                qr += 1
-
-    return background
+    for i in range(2):
+        for y in range(20, background1.size[1] - 800, 820):
+            for x in range(20, background1.size[0] - 800, 1620):
+                if qr < len(images) and qr < 6:
+                    backs[0].paste(images[qr], (x, y))
+                    qr += 1
+                elif qr < len(images):
+                    backs[1].paste(images[qr], (x, y))
+                    qr += 1
+    return backs
 
 
 if __name__ == '__main__':
-    back = get_a4(['10010001', '10010002', '10010003', '10010004'])
-    back.show()
+    back = get_a4(['10010001', '10010002', '10020003', '10010004', '20010005', '10010006', '10010007'])
+    back[0].show()
