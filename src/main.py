ebe61f653b3e268892fbf24316cf3c09e6c1456b
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-12-07 19:41:37 +0300

    update form_tickets_button_clicked method

--- src/main.py
+++ src/main.py
@@ -16,6 +16,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+import tempfile
+import platform
+import os
 from PyQt5 import QtWidgets
 from sql_utils import *
 import mainwindow
@@ -120,7 +123,12 @@ class TicketAgencyApp(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
 
     def form_tickets_button_clicked(self):
         tickets = get_a4(self.order_list)
-        tickets.show()
+        tmp = tempfile.NamedTemporaryFile(delete=False)
+        tickets[0].save(tmp, format='PDF', resolution=100.0, save_all=True, append_images=tickets[1:])
+        if platform.system() == 'Linux':
+            os.system('xdg-open ' + tmp.name)
+        elif platform.system() == 'Windows':
+            os.system(tmp.name)
 
     def cancel_button_clicked(self):
         if not self.order_is_payed:
